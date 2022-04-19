a5833c51c4a02080dfe6270e16746b31a8417926
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-04-19 21:48:27 +0200

    codigo bloque

---
+++ Clases/Ejercicio1/Bloque.py
@@ -0,0 +1,9 @@
+class Bloque: 
+    # Un bloque es un conjunto de instrucciones ejecutadas 
+    # unas detrás de otras. 
+    def __init__(self): 
+        # Por defecto, un bloque no contiene ninguna instrucción. 
+        self.instrucciones = [] 
+ 
+    def agregarInstruction(self, instruccion): 
+        self.instrucciones.append(instruccion) 
