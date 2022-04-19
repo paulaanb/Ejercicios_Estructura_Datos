b8072de58f0b48b007dcb15443330218f99fff68
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-04-19 21:49:03 +0200

    codigo si

---
+++ Clases/Ejercicio1/Si.py
@@ -0,0 +1,11 @@
+class Si: 
+    # Representa una instrucción 'if'. 'condicion' es una cadena 
+    # de caracteres que contiene la evaluación de la condición, 
+    # 'entonces' es el bloque de instrucciones ejecutadas si la condición 
+    # se verifica, 'si_no' es el bloque de instrucciones ejecutadas 
+    # si no se verifica. 
+ 
+    def __init__(self, condicion, entonces, si_no): 
+        self.condicion = condicion 
+        self.entonces = entonces 
+        self.si_no = si_no 
