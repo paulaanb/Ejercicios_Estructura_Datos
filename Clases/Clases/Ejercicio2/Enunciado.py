df5fcd6173b9b329d0d0a38989008365004ea5e7
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-04-20 17:57:25 +0200

    creamos el archivo frases.txt

--- Clases/Clases/Ejercicio2/Enunciado.py
+++ Clases/Clases/Ejercicio2/Enunciado.py
@@ -1,1 +1,3 @@
 #Enunciado: siguiendo la filosofía MVC, escriba un programa que lea dos líneas en la entrada estándar, las convierta a mayúsculas y las escriba en un archivo. Tenga en cuenta que para beneficiarse plenamente de las ventajas del design pattern MVC, los atributos, en particular los del modelo, se deben encapsular.
+
+
