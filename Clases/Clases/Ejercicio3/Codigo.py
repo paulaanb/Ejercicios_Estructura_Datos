b60b322e5121bc0e055ad1eab7329f0abdaa6dba
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-04-20 18:03:11 +0200

    codigo 3

--- Clases/Clases/Ejercicio3/Codigo.py
+++ Clases/Clases/Ejercicio3/Codigo.py
@@ -1,2 +1,21 @@
 #Enunciado: Implementar un programa que factura productos por valor de 100, con la tasa de IVA correcta, según sean productos de alimentación o servicios.
 
+class Naturaleza:
+    def __init__(self):
+        self.alimentaria = 0.055 #5,5%
+        self.servicio = 0.2 #20%
+
+
+class Producto(Naturaleza):
+    def __init__(self, tasa_iva):
+        self.tasa_iva = tasa_iva
+    def facturar(self):
+        return 100 + 100*self.tasa_iva
+
+
+class FactoryFactura(Producto):
+    def __init__(self):
+        pass
+    def crear(self):
+        clase = Producto(self.tasa_iva)
+        return clase
